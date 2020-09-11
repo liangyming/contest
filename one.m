2420bda81c89c8bbec240d237ff1fdda9fad8a0d
HQHollow <1354252799@qq.com>
2020-09-11 17:12:48 +0800

    9/11 17:12

--- one.m
+++ one.m
@@ -5,12 +5,22 @@ v = 70;
 v = v / 60;
 len = v * data(:, 1);
 data(:, 3) = len;
-for i = 1:709
-    if (len(i)-25) < (35.5*5 - 2.5)
-    t = 175;
-    elseif condition
-        
+%循环求解参数an
+an = [];
+for i = 1:708
+    %不同区域加热温度t不同
+    if (len(i) - 25) < (35.5*5 - 2.5)
+        t = 175;
+    elseif (len(i) - 25) < (35.5*6 - 2.5)
+        t = 195;
+    elseif (len(i) - 25) < (35.5*7 - 2.5)
+         t = 235;
+    elseif (len(i) - 25) < (35.5*9 - 2.5)
+        t = 255;
     else
-         
+        t = 25;
     end
+    a = w / (t - data(i, 2)) * (data(i+1,2) - data(i,2)) / 0.5;
+    an = [an, a];
 end
+disp(mean(an));
