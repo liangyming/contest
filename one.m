6bc56fa4c5cec6de3e91d8e9d27a608fc406d2cc
HQHollow <1354252799@qq.com>
2020-09-11 16:46:34 +0800

    9/11 16:46

---
+++ one.m
@@ -0,0 +1,16 @@
+%µÚÒ»Ìâ
+data = xlsread("fujian.xlsx");
+w = 0.015
+v = 70;
+v = v / 60;
+len = v * data(:, 1);
+data(:, 3) = len;
+for i = 1:709
+    if (len(i)-25) < (35.5*5 - 2.5)
+    t = 175;
+    elseif condition
+        
+    else
+         
+    end
+end
